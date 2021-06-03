9c7d904168cd5aa57bd91efe3f9b94c8bf89ab0e
Luphia Chang <luphia@outlook.com>
2021-06-03 14:48:59 +0800

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -18,7 +18,7 @@ sudo apt-get install openssl libtool autoconf automake uuid-dev build-essential 
 ### Install Golang
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
-sudo apt-get install golang-go
+sudo apt-get install golang-go -y
 
 ### Setup TideBit Chain
 mkdir ~/workspace
