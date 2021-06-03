916500b0c8c653b59c6e853c27a71941a88eeff6
Luphia Chang <luphia@outlook.com>
2021-06-03 14:46:59 +0800

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -21,8 +21,13 @@ sudo apt-get update
 sudo apt-get install golang-go
 
 ### Setup TideBit Chain
+mkdir ~/workspace
+sudo mkdir ~/TideBit-Chaindata
+cd ~/workspace
 git clone https://github.com/BOLT-Protocol/TideBit-Chain
 cd TideBit-Chain
 make all
 sudo ln -s ./build/bin/geth /usr/local/bin/
 sudo ln -s ./build/bin/puppeth /usr/local/bin/
+geth init genesis.json --datadir ~/TideBit-Chaindata
+geth --datadir ~/TideBit-Chaindata --rpc --http --http.addr 0.0.0.0 --http.api debug,net,eth,shh,web3,txpool --ws --ws.addr 0.0.0.0 --ws.origins "*" --ws.port 8545 --ws.api eth,net,web3,network,debug,txpool --gcmode=archive
